6be6cce4982ef51cc1186d8052a20d5045fb4826
Daniel Nishi <dhnishi@gmail.com>
2015-01-27 16:02:47 -0800

    Added channel hopping to monitoring.

--- wifimonitor.py
+++ wifimonitor.py
@@ -1,7 +1,13 @@
+import sys, os, signal
 from scapy.all import *
+from multiprocessing import Process
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
+
+# Set non-statically in the future.
+iface = 'wlan1'
+channel = 1
 
 def PacketHandler(pkt):
 	if pkt.haslayer(Dot11):
@@ -16,4 +22,28 @@ def AllPacketHandler():
 				print "MAC address: %s" % (pkt.addr2)
 	return packetHandler
 
-sniff(iface='wlan1', prn = AllPacketHandler())
+def hop_channels():
+	channel = 0
+	while True:
+		try:
+			channel = (channel + 1) % 14
+			os.system('iw dev %s set channel %d' % (iface, channel))
+			print 'setting channel to %d' % (channel)
+			time.sleep(5)
+		except Exception as e:
+			print "Channel hopping ceased."
+			print e
+			break
+
+def signal_handler():
+	p.terminate()
+	p.join()
+	print "Terminating monitoring."
+	sys.exit(0)
+
+if __name__ == '__main__':
+	print "Beginning packet capture..."
+	p = Process(target = hop_channels)
+	p.start()
+	signal.signal(signal.SIGINT, signal_handler)
+	sniff(iface=iface, prn = AllPacketHandler())
