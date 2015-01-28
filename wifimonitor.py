a5c5e7a260e46ab3300e5cb7cb4d8c96a6763f40
Daniel Nishi <dhnishi@gmail.com>
2015-01-28 12:39:18 -0800

    Improved outputs.

--- wifimonitor.py
+++ wifimonitor.py
@@ -1,3 +1,8 @@
+'''
+In order to properly run, ensure that |iface| is in monitor mode.
+Furthermore, run this Python script as sudo.
+'''
+
 import sys, os, signal
 from scapy.all import *
 from multiprocessing import Process
@@ -9,24 +14,32 @@ PROBE_REQUEST_SUBTYPE = 4
 iface = 'wlan1'
 channel = 1
 
+seen_mac_addresses = set()
+seen_SSIDs = set()
+
 def PacketHandler(pkt):
 	if pkt.haslayer(Dot11):
 		if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE:
-			print "MAC address: %s, SSID: %s" % (pkt.addr2, pkt.getlayer(Dot11ProbeReq).info)
+			seen_mac_addresses.add(pkt.addr2)
+			if pkt.getlayer(Dot11ProbeReq).info not in seen_SSIDs:
+				seen_SSIDs.add(pkt.getlayer(Dot11ProbeReq).info)
+			if pkt.getlayer(Dot11ProbeReq).info:
+				print "MAC address: %s, SSID: %s" % (pkt.addr2, pkt.getlayer(Dot11ProbeReq).info)
 
 def AllPacketHandler():
 	data = {'last_mac': None}
 	def packetHandler(pkt):
-		if pkt.haslayer(Dot11) and pkt.addr2 != data['last_mac']:
+		if pkt.haslayer(Dot11) and pkt.addr2 != data['last_mac'] and pkt.addr2 not in seen_mac_addresses:
 				data['last_mac'] = pkt.addr2
-				print "MAC address: %s" % (pkt.addr2)
+				seen_mac_addresses.add(pkt.addr2)
+				print "MAC address detected: %s" % (pkt.addr2)
 	return packetHandler
 
 def hop_channels():
 	channel = 0
 	while True:
 		try:
-			channel = (channel + 1) % 14
+			channel = (channel % 14 + 1)
 			os.system('iw dev %s set channel %d' % (iface, channel))
 			print 'setting channel to %d' % (channel)
 			time.sleep(5)
@@ -35,10 +48,18 @@ def hop_channels():
 			print e
 			break
 
-def signal_handler():
+def signal_handler(signal, frame):
 	p.terminate()
 	p.join()
 	print "Terminating monitoring."
+	print "Now displaying detected IP addresses..."
+	for address in seen_mac_addresses:
+		print address
+
+	print "Now printing observed SSID probe requests"
+	for ssid in seen_SSIDs:
+		print ssid
+
 	sys.exit(0)
 
 if __name__ == '__main__':
@@ -46,4 +67,4 @@ if __name__ == '__main__':
 	p = Process(target = hop_channels)
 	p.start()
 	signal.signal(signal.SIGINT, signal_handler)
-	sniff(iface=iface, prn = AllPacketHandler())
+	sniff(iface=iface, prn = PacketHandler)
