9f64ff6451f9e200349f08bb93617aa74b8bbc3f
Daniel Nishi <dhnishi@gmail.com>
2015-01-29 20:47:20 -0800

    Database commit.

---
+++ database.py
@@ -0,0 +1,30 @@
+import os, sys, datetime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+'''
+class MACAddress(Base):
+	__tablename__ = 'mac'
+	# We need an id as address is not a good primary key, due to mutability.
+	id = Column(Integer, primary_key=True)
+	address = Column(String(17), nullable=False)
+
+class SSID(Base):
+	__tablename__ = 'ssid'
+	id = Column(Integer, primary_key=True)
+	# SSIDs have a 31 or 32 character limit in name length.
+	name = Column(String(32), nullable=False)
+'''
+class Packet(Base):
+	__tablename__ = 'packet'
+	id = Column(Integer, primary_key=True)
+	mac = Column(String(17), nullable=False)
+	ssid = Column(String(32))
+	time = Column(DateTime, default=datetime.datetime.now())
+	signal = Column(Integer)
+
+def createNewDatabase(filename):
+	engine = create_engine('sqlite:///%s.db' % filename)
+	Base.metadata.create_all(engine)
--- wifimonitor.py
+++ wifimonitor.py
@@ -1,11 +1,25 @@
 '''
 In order to properly run, ensure that |iface| is in monitor mode.
 Furthermore, run this Python script as sudo.
+
+Dependencies: Scapy, SQLite, SQLAlchemy.
 '''
 
 import sys, os, signal
 from scapy.all import *
 from multiprocessing import Process
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database import Base, Packet
+
+
+# Database setup.
+# TODO: Make this mutable.
+engine = create_engine('sqlite:///example.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
@@ -19,13 +33,10 @@ seen_SSIDs = set()
 
 def PacketHandler(pkt):
 	if pkt.haslayer(Dot11):
-		if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE:
+		if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE and pkt.getlayer(Dot11ProbeReq).info != "":
 			seen_mac_addresses.add(pkt.addr2)
-			if pkt.getlayer(Dot11ProbeReq).info not in seen_SSIDs:
-				seen_SSIDs.add(pkt.getlayer(Dot11ProbeReq).info)
-			if pkt.getlayer(Dot11ProbeReq).info:
-				print -(256-ord(pkt.notdecoded[-4:-3]))
-				print "MAC address: %s, SSID: %s" % (pkt.addr2, pkt.getlayer(Dot11ProbeReq).info)
+			seen_SSIDs.add(pkt.getlayer(Dot11ProbeReq).info)
+			print "MAC address: %s, SSID: %s, Signal Strength(-100 to 0): %d" % (pkt.addr2, pkt.getlayer(Dot11ProbeReq).info, -(256-ord(pkt.notdecoded[-4:-3])))
 
 def AllPacketHandler():
 	data = {'last_mac': None}
@@ -37,6 +48,15 @@ def AllPacketHandler():
 				print "MAC address detected: %s" % (pkt.addr2)
 	return packetHandler
 
+def PrintPackets(pkt):
+	if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE:
+		mac_address = pkt.addr2
+		signal = -(256-ord(pkt.notdecoded[-4:-3]))
+		ssid = pkt.getlayer(Dot11ProbeReq).info
+		new_packet = Packet(mac=mac_address,ssid=ssid,signal=signal)
+		session.add(new_packet)
+		session.commit()
+		print "MAC address: %s, SSID: %s, Signal Strength(-100 to 0): %d" % (mac_address, ssid, signal)
 
 '''
 Channel hopper.
@@ -66,10 +86,13 @@ def signal_handler(signal, frame):
 	for ssid in seen_SSIDs:
 		print ssid
 
+	print "-- %d MAC addresses detected --" % len(seen_mac_addresses)
+	print "-- %d SSIDs addresses detected --" % len(seen_SSIDs)
 	sys.exit(0)
 
 
 def set_iface_to_monitor_mode(interface):
+	'''
 	try:
 		# TODO(dhnishi): Make this fail if an ifconfig or iwconfig fails.
 		err = os.system('ifconfig %s down' % interface)
@@ -79,8 +102,11 @@ def set_iface_to_monitor_mode(interface):
 	except Exception as e:
 		print e
 		return False
+	'''
+	return True
 
 if __name__ == '__main__':
+	# TODO: Don't hardcore the interface.
 	print "Starting interface into monitor mode..."
 	if not set_iface_to_monitor_mode(iface):
 		print "Error starting %s into monitor mode" % iface
@@ -90,4 +116,4 @@ if __name__ == '__main__':
 	p = Process(target = hop_channels)
 	p.start()
 	signal.signal(signal.SIGINT, signal_handler)
-	sniff(iface=iface, prn = AllPacketHandler())
+	sniff(iface=iface, prn = PrintPackets, store=0)
