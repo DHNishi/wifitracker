cf90a45b9021157d7600af263589e617a0b5146f
dhnishi <dhnishi@gmail.com>
2015-03-05 18:11:37 -0800

    Add some polish.

--- frontend/app/server.py
+++ frontend/app/server.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from app import app
 import os
 from functools import wraps
+from collections import defaultdict
 
 # Mock stuff.
 import mock_database
@@ -56,22 +57,50 @@ def index():
 def packetView():
     title = 'Packet-by-Packet View'
     template = 'packetView.html'
-    packets = session.query(database.Packet)
-    return render_template(template, title=title, packets=packets)
+
+    maybe_ssid = request.args.get("ssid")
+    maybe_mac = request.args.get("mac")
+
+    packets = _filterPackets(maybe_ssid, maybe_mac)
+    #packets = session.query(database.Packet)
+    return render_template(template, title=title, packets=packets, ssid=maybe_ssid, mac=maybe_mac)
+
+def _filterPackets(ssid, mac):
+    query = session.query(database.Packet)
+    if ssid is not None:
+        query = query.filter(database.Packet.ssid == ssid)
+    if mac is not None:
+        query = query.filter(database.Packet.mac == mac)
+    return query
 
 @app.route('/densityGraph')
 @databaseUploadRequired
 def densityGraph():
     title = 'Density Graph'
     template = 'densityGraph.html'
-    packets = mock_database.getPackets(10)
+    packets = session.query(database.Packet)
+    hour_count = countByHour(packets)
 
     # Attempt to get the real database.
     initializeDatabaseIfNotInitialized();
+    return render_template(template, title=title, packets=packets, hour_count=hour_count)
 
+"""
+countByHour creates a count of unique MAC addresses encountered and breaks it down by hour.
+"""
+def countByHour(packets):
+    hour_counter = defaultdict(set)
+    for packet in packets:
+        hour_counter[packet.time.hour].add(packet.mac)
+    hour_counter[0].add("lolol")
 
+    visitors = {h: len(v) for (h, v) in hour_counter.iteritems()}
+    for i in xrange(0,24):
+        if i not in visitors:
+            visitors[i] = 0
 
-    return render_template(template, title=title, packets=packets)
+    print visitors
+    return visitors 
 
 @app.route('/loadDatabase', methods=['GET', 'POST'])
 def uploadDatabase():
